f749138032f66672f688077de62050c8e18057fd
ecenazdogan <c2011060@student.cankaya.edu.tr>
2024-02-28 22:27:59 +0300

    Initial commit

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Asteroid : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+
+    public float size = 1f;
+    public float minSize = 0.35f;
+    public float maxSize = 1.65f;
+    public float movementSpeed = 50f;
+    public float maxLifetime = 30f;
+    public Player player;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        GameObject scoreGO = GameObject.Find("Player");
+        player = scoreGO.GetComponent<Player>();
+        //make each asteroid different
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360f);
+        //make size and mass of the asteroid different based on their assigned random value above
+        transform.localScale = Vector3.one * size;
+        rigidbody.mass = size;
+        //destroy when max life time is reached
+        Destroy(gameObject, maxLifetime);
+
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        //add force to asteroid
+        rigidbody.AddForce(direction * movementSpeed);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            //check if asteroid is large enough to split in half
+            if ((size * 0.5f) >= minSize)
+            {
+                CreateSplit();
+                CreateSplit();
+            }
+            //destroy the current asteroid
+            Destroy(this.gameObject);
+            
+            //Player[] players = FindObjectsOfType<Player>();
+            //Player player = players.[0];
+            //var objects = GameObject.FindGameObjectsWithTag("Player");
+            if (size < 0.7f)
+            {
+                player.score += 25; // small asteroid
+            }
+            else if (size < 1.4f)
+            {
+                player.score += 10;// medium asteroid
+            }
+            else
+            {
+                player.score += 5;// large asteroid
+            }
+        }
+
+    }   
+
+    private Asteroid CreateSplit()
+    {
+        //set the new asteroid position but slightly different so they don't spawn on each other
+        Vector2 position = transform.position;
+        position += Random.insideUnitCircle * 0.5f;
+        //create the new asteroid but half the sized
+        Asteroid half = Instantiate(this, position, transform.rotation);
+        half.size = size * 0.5f;
+        half.SetTrajectory(Random.insideUnitCircle.normalized);
+        return half;
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Bullet bulletPrefab;
+
+    public float thrustSpeed = 1f;
+    public bool thrusting { get; private set; }
+
+    public float turnDirection { get; private set; } = 0f;
+    public float rotationSpeed = 0.1f;
+
+    public float respawnDelay = 2f;
+    public float respawnInvulnerability = 3f;
+
+    public ParticleSystem explosionEffect;
+
+    public int score=0;
+    static private int highscore = 0;
+    public int defaultlives = 3;
+    public int livesCounter;
+    public TMP_Text livesText;
+    public TMP_Text scoreText;
+    public TMP_Text highscoreText;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            turnDirection = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            turnDirection = -1f;
+        }
+        else{
+            turnDirection = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Shoot();
+        }
+        livesText.text = "x " + livesCounter;
+        if (livesCounter < 1)
+        {
+            GameOver();
+        }
+        scoreText.text = "Score: " + score;
+        highscoreText.text = "High Score: " + highscore;
+        if (score > highscore)
+        {
+            highscore = score;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (thrusting)
+        {
+            rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+
+        if (turnDirection != 0f)
+        {
+            rigidbody.AddTorque(this.rotationSpeed * turnDirection);
+        }
+    }
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, transform.position, transform.rotation);
+        bullet.Project(transform.up);
+    }
+    private void TurnOnCollisions()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Asteroid")){
+            TakeLife();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = 0f;
+            gameObject.SetActive(false);
+            PlayerDeath();
+        }
+    }
+    public void PlayerDeath()
+    {
+        explosionEffect.transform.position = transform.position;
+        explosionEffect.Play();
+        this.gameObject.SetActive(false);
+        Invoke(nameof(Respawn), respawnDelay);
+    }
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        explosionEffect.transform.position = asteroid.transform.position;
+        explosionEffect.Play();
+    }
+    private void Start()
+    {
+        NewGame();
+        score = 0;
+        defaultlives = 3;
+        livesCounter = defaultlives;
+    }
+    public void NewGame()
+    {
+        Asteroid[] asteroids = FindObjectsOfType<Asteroid>();
+        for (int i = 0; i < asteroids.Length; i++)
+        {
+            Destroy(asteroids[i].gameObject);
+        }
+        Respawn();
+    }
+    public void Respawn()
+    {
+        this.transform.position = Vector3.zero;
+        this.gameObject.layer = LayerMask.NameToLayer("Ignore Collisions");
+        this.gameObject.SetActive(true);
+        Invoke(nameof(TurnOnCollisions), respawnInvulnerability);
+    }
+    public void TakeLife()
+    {
+        livesCounter--;
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void HighScore() 
+    {
+        if (score > highscore)
+        {
+            highscore = score;
+        }
+    }
+
+}
